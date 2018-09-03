5babfb3ab4127d01c0ef95dbbfdcec3e138dcc8f
“[Kram <[kramarenko905@gmail.com]>
2018-09-03 21:44:25 +0300

    WEB-010 Removed project "Client"

--- Client.ReactJS/app/src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import store from './reducers/store';
-import articles from './reducers/articles';
-
-export default combineReducers({
-    store, articles
-});
