ab692d49ed3450d48dfb20883071785cfbe558ef
“[Kram <[kramarenko905@gmail.com]>
2018-11-02 11:56:23 +0200

    WEB-017 Removed "Articles.ReadSide.Service" project since it is not used anymore.

--- Infrastructure/Exception/MessagingException.cs
+++ Infrastructure/Exception/MessagingException.cs
@@ -3,29 +3,28 @@
 namespace Infrastructure
 {
     public class MessagingException : Exception
-	{
-		public MessagingExceptionType Type { get; }
-		public Type SourceType { get; }
-        public string Message { get; set; }
+    {
+        public MessagingExceptionType Type { get; }
+        public Type SourceType { get; }
 
         public MessagingException(MessagingExceptionType type,
-										Type sourceType = null,
-										string message = null)
-		{
-			Type = type;
-			SourceType = sourceType;
-			Message = message;
-		}
-	}
+                                        Type sourceType = null,
+                                        string message = null)
+            : base(message)
+        {
+            Type = type;
+            SourceType = sourceType;
+        }
+    }
 
-	public enum MessagingExceptionType
-	{
-		NullCommand = 0,
-		NullEvent = 1,
-		InvalidCommand = 2,
-		InvalidEvent = 3,
-		EventStoreConcurency = 4,
-		AggregateNotFound = 5,
-		AggregateDeleted = 6
-	}
+    public enum MessagingExceptionType
+    {
+        NullCommand = 0,
+        NullEvent = 1,
+        InvalidCommand = 2,
+        InvalidEvent = 3,
+        EventStoreConcurency = 4,
+        AggregateNotFound = 5,
+        AggregateDeleted = 6
+    }
 }
